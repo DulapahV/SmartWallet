cde491e689c5506083547b7d05d263d994f76ec7
Anucha Chewachanon <68524576+SpiralNuggets@users.noreply.github.com>
2022-11-03 20:12:05 +0700

    Getters and Setters for TaskInstance
    also changed from the deprecated to hell and back Date to LocalDate

--- MyStudyPlan/src/main/java/MyStudyPlan/TaskInstance.java
+++ MyStudyPlan/src/main/java/MyStudyPlan/TaskInstance.java
@@ -1,5 +1,7 @@
 package MyStudyPlan;
 
+import java.time.LocalDate;
+
 enum TaskType {
    Assignment, Reminder, Revision
 }
@@ -8,14 +10,54 @@ public class TaskInstance {
     private String subject;
     private String title;
     private TaskType type;
-    private Date dueDate;
+    private LocalDate dueDate;
     private String description;
 
-    public TaskInstance(String subject, String title, TaskType type, Date dueDate, String description) {
+    public TaskInstance(String subject, String title, TaskType type, LocalDate dueDate, String description) {
         this.subject = subject;
         this.title = title;
         this.type = type;
         this.dueDate = dueDate;
+        this.description = description;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public TaskType getType() {
+        return type;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setType(TaskType type) {
+        this.type = type;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
